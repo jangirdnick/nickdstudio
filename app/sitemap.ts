fd8436018d6f9c214528d664b53ddca263daf1cf
its-nandlal <147331160+its-nandlal@users.noreply.github.com>
2025-08-14 21:06:48 +0530

    therd commit

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,43 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://nickdstudio.com',
+      url: 'https://nickdstudio.online',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://nickdstudio.com/services',
+      url: 'https://nickdstudio.online/services',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://nickdstudio.com/about',
+      url: 'https://nickdstudio.online/about',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
-      url: 'https://nickdstudio.com/contact',
+      url: 'https://nickdstudio.online/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://nickdstudio.online/privacy-policy',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://nickdstudio.online/projects',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://nickdstudio.online/terms-conditions',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
