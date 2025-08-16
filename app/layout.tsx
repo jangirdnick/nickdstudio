c79d970451de6adbbd3df9f870a1eec70e3445fc
its-nandlal <147331160+its-nandlal@users.noreply.github.com>
2025-08-16 21:22:31 +0530

    Six commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,9 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
+    "max-snippet": -1,
+    "max-video-preview": -1,
+    "max-image-preview": "large",
     googleBot: {
       index: true,
       follow: true,
